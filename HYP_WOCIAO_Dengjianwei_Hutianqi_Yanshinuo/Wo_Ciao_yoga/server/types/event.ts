97b86477d7b5a9041b9c2410bfeb262dca82dd20
Jianwei Deng <164878825+Simon0619@users.noreply.github.com>
2025-07-03 14:43:36 +0200

    Delete HYP_WOCIAO_Dengjianwei_Hutianqi_Yanshinuo directory

--- HYP_WOCIAO_Dengjianwei_Hutianqi_Yanshinuo/Wo_Ciao_yoga/server/types/event.ts
+++
@@ -1,9 +0,0 @@
-import type { InferSelectModel } from 'drizzle-orm'
-import type { events, teachers } from '~/server/database/schema'
-
-export type Event = InferSelectModel<typeof events>
-export type Teacher = InferSelectModel<typeof teachers>
-
-export interface EventWithTeacher extends Event {
-  teachers: Teacher[]
-}
--- HYP_WOCIAO_Dengjianwei_Hutianqi_Yanshinuo/Wo_Ciao_yoga/utils/common.ts
+++
@@ -1,3 +0,0 @@
-export function formatDate(date: number) {
-  return new Date(date * 1000).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })
-}
